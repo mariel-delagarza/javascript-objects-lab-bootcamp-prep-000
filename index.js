28acd5bf5323b979211312a4c472111c5bf2f294
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2018-09-13 17:17:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 var recipes = {}
 function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign({}, object, {[key]: value })
+  return Object.assign({}, object, {[key]: value });
 }
 function destructivelyUpdateObjectWithKeyAndValue (object, key, value) {
-  object[key] = value
-  return object 
+  object[key] = value;
+  return object;
 }
 function deleteFromObjectByKey (object, key) {
- return Object.assign({}, delete object.key)
+ return Object.assign({}, delete object.key);
 } 
 function destructivelyDeleteFromObjectByKey (object, key) {
- delete object[key]
- return object 
+ delete object[key];
+ return object;
 }
 
   
